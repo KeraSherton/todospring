d5bdfdfe104d31f113592d79e33fc703956688bf
Justyna Gajdek <test@email.com>
2025-08-13 14:18:22 +0000

    add get and post methods

--- src/main/java/com/neueda/todoapp/controller/TodoController.java
+++ src/main/java/com/neueda/todoapp/controller/TodoController.java
@@ -1,10 +1,26 @@
 package com.neueda.todoapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.neueda.todoapp.model.Task;
+import com.neueda.todoapp.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/todo")
 public class TodoController {
+
+@Autowired
+    private TodoService todoService;
+
+@GetMapping("/")
+    public List<Task> getAllTask(){
+    return todoService.getAllTasks();
+}
+@PostMapping
+    public Task getTask(@PathVariable String id) {
+    return todoService.getTask(id);
+}
 
 }
